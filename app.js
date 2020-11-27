d3d6b93a277fd5f3a40abf10610ebd3b4d363b0c
Mrinal Das <61489329+themrinal@users.noreply.github.com>
2020-11-27 11:51:13 +0530

    First upload

---
+++ app.js
@@ -0,0 +1,28 @@
+require('./models/db');
+
+const path = require('path');
+const exphbs = require('express-handlebars');
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
+
+app.use(bodyParser.urlencoded({extended : true}));
+app.use(bodyParser.json());
+app.set('views', path.join(__dirname, './views/'));
+app.engine('hbs', exphbs({
+  extname : 'hbs',
+  defaultLayout : 'mainLayout',
+  layoutsDir : __dirname + '/views/layouts',
+  runtimeOptions : {
+    allowProtoPropertiesByDefault : true,
+    allowProtoMethodsByDefault : true
+  }
+}));
+app.set('view engine', 'hbs');
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, console.log(`Server is running at port ${PORT}`));
+
+const employeeController = require('./controllers/employeeController');
+app.use('/employee', employeeController);
