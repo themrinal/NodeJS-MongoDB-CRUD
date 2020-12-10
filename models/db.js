615b62d2876ef3354f47e027ec0696c956d9893b
Mrinal Das <61489329+themrinal@users.noreply.github.com>
2020-12-10 13:54:37 +0530

    Update db.js

--- models/db.js
+++ models/db.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-const URI = "mongodb+srv://mrinaldas:Mrinal2020@cluster0.dawn7.mongodb.net/EmployeeDB?retryWrites=true&w=majority";
+const URI = "mongodb+srv://<username>:<passwprd>@cluster0.dawn7.mongodb.net/<databasename>?retryWrites=true&w=majority";
 
 mongoose.connect(URI, {useNewUrlParser : true, useUnifiedTopology : true, useFindAndModify : false}, (err) => {
   if(err) {
